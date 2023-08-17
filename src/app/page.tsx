76f1d84c2742f82172cd430c02bdf304dc18890a
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-17 14:18:29 +0100

    View Counter script setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://api.github.com/repos/joy-ling/view-counter");
+  const data = await res.json();
+  console.log(data);
+
   return (
     <main className={styles.main}>
         <p>0</p>
