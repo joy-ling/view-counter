44a30e0366aa3d1cdc14e5101f9a33e9a062abf2
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-17 14:27:03 +0100

    Added revalidation param

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default async function Home() {
-  const res = await fetch("https://api.github.com/repos/joy-ling/view-counter");
+  const res = await fetch("https://api.github.com/repos/joy-ling/view-counter", {next: {revalidate: 5}});
   const data = await res.json();
   console.log(data);
 
   return (
     <main className={styles.main}>
-        <p>0</p>
+        {data.stargazers_count}
     </main>
   )
 }
