93fdbc8d97efb3813b7a72a442c874b4cd373719
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-17 14:13:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
         <p>0</p>
-      </div>
     </main>
   )
 }
