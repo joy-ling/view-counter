81eba52c0ec85bd055cf191842c08ec5bf083551
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-17 14:34:13 +0100

    Remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default async function Home() {
-  const res = await fetch("https://api.github.com/repos/joy-ling/view-counter", {next: {revalidate: 5}});
+  const res = await fetch("https://api.github.com/repos/joy-ling/view-counter", {next: {revalidate: 5}}); // Second param revalidates every 5 seconds
   const data = await res.json();
-  console.log(data);
 
   return (
     <main className={styles.main}>
