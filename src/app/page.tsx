3a0dd82fa843006d07e20b1a70f84300adefc2cd
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-17 15:25:21 +0100

    Added view counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { kv } from "@vercel/kv";
 
 export default async function Home() {
+  // See how many people like us on Github
   const res = await fetch("https://api.github.com/repos/joy-ling/view-counter", {next: {revalidate: 5}}); // Second param revalidates every 5 seconds
   const data = await res.json();
 
+  // See how many times this page has been viewed
+  const pageViews = await kv.incr("views")
+
   return (
     <main className={styles.main}>
-        <p>0</p>
+        <p>{pageViews}</p>
         <span>⭐ {data.stargazers_count} ⭐</span>
     </main>
   )
